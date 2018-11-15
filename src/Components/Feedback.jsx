aa95e181714eeaa3233ca824da643a52a8ddfdf4
leslie-alldridge <leslie.alldridge@gmail.com>
2018-11-15 17:58:15 +1300

    working on evt listener

--- src/Components/Feedback.jsx
+++ src/Components/Feedback.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 
 class FeedBack extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tools: "no selection",
+      knowledge: "no selection",
+      other: "no additional feedback"
+    };
+    this.formUpdate = this.formUpdate.bind(this);
+  }
+
+  formUpdate(e) {
+    console.log(e.target.name);
+    let name = e.target.name;
+    this.setState({
+      [name]: !this.state[name]
+    });
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="item">
-        <p>Feedback</p>
+        <p>Do you have the rights tools to provide world class support?</p>
+        <button name="yes">Yes</button>
+        <button name="no">No</button>
+        <div
+          class="ui submit button"
+          name="knowledge"
+          onClick={this.formUpdate}
+        >
+          Submit
+        </div>
       </div>
     );
   }
